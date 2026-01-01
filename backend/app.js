3863e99cd7f9b054fdc77fa352a3f877c9dd983d
Shivam Kumar <shivamku9107@gmail.com>
2026-01-01 21:44:19 +0530

    Adding Optional Port

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const insertData = require("./mdb")
 // const mysqlInsert = require("./db")
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // Middleware to parse JSON and URL-encoded data
